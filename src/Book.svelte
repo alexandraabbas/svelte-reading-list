7d802fb7b746269f33dd7c3d5d1009612ed36d70
Alexa Abbas <alexa.abbas@b-0853.mynet>
2020-04-11 21:53:39 +0100

    Add label to book

--- src/Book.svelte
+++ src/Book.svelte
@@ -6,6 +6,7 @@
   let imageLink = book.imageLink;
   let title = book.title;
   let authors = book.authors;
+  let label = book.label;
 
   function del() {
     db.collection("books")
@@ -18,6 +19,36 @@
         console.error("Error removing document: ", error);
       });
   }
+
+  function update(label) {
+    db.collection("books")
+      .doc(book.id)
+      .set(
+        {
+          label: label
+        },
+        { merge: true }
+      )
+      .then(function() {
+        console.log("Document successfully updated!");
+      })
+      .catch(function(error) {
+        console.error("Error updating document: ", error);
+      });
+  }
+
+  function updateLabel() {
+    if (label == "To Read") {
+      label = "Reading";
+      update(label);
+    } else if (label == "Reading") {
+      label = "Finished";
+      update(label);
+    } else {
+      label = "To Read";
+      update(label);
+    }
+  }
 </script>
 
 <style>
@@ -25,6 +56,7 @@
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
     transition: 0.3s;
     border-radius: 5px;
+    position: relative;
   }
   .card:hover {
     box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
@@ -35,11 +67,60 @@
   img {
     border-radius: 5px 5px 0 0;
     height: 189px;
+    margin-top: 12px;
+  }
+  .label {
+    font-size: 12px;
+    padding: 2px 6px;
+    border-radius: 100px;
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin: 12px;
+    cursor: pointer;
+  }
+  .toread {
+    color: white;
+    background-color: #008cba;
+    border: 2px solid #008cba;
+    box-shadow: 1px 1px 0 #034f86;
+  }
+  .toread:active {
+    background-color: #034f86;
+    border: 2px solid #034f86;
+  }
+  .reading {
+    color: white;
+    background-color: #4caf50;
+    border: 2px solid #4caf50;
+    box-shadow: 1px 1px 0 #306532;
+  }
+  .reading:active {
+    background-color: #306532;
+    border: 2px solid #306532;
+  }
+  .finsihed {
+    color: black;
+    background-color: #e7e7e7;
+    border: 2px solid #e7e7e7;
+    box-shadow: 1px 1px 0 #8c8787;
+  }
+  .finsihed:active {
+    background-color: #8c8787;
+    border: 2px solid #8c8787;
   }
 </style>
 
 <li>
   <div class="card">
+    <button
+      class="label"
+      class:toread={label === 'To Read'}
+      class:reading={label === 'Reading'}
+      class:finsihed={label === 'Finished'}
+      on:click={updateLabel}>
+      {label}
+    </button>
     <img src={imageLink} alt="Thumbnail" />
     <div class="container">
       <a href={infoLink}>
